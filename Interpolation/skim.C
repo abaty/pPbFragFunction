6ddd7730bdf8018d8087329fd5991088203a210c
Austin A Baty <abaty@hidsk0001.cmsaf.mit.edu>
2015-01-19 20:24:03 -0500

    updated gluon fraction code

--- Interpolation/skim.C
+++ Interpolation/skim.C
@@ -2,64 +2,135 @@
 #include "TH1D.h"
 #include "TH2D.h"
 #include "TLorentzVector.h"
-#include "HiForestAnalysis/hiForest.h"
+#include "HiForestAnalysis/hiForest_gluonfrac.h"
 #include <iostream>
 
-void skim()
+//mode 0 - 2.76 tev pp
+//mode 1 - 5.02 tev pPb
+
+void skim(int mode = 0)
 {
   TH1D::SetDefaultSumw2();
   TH2D::SetDefaultSumw2();
 
-  const int nbins = 9;
-  float crossSection[nbins+1] = {2.034E-01,1.075E-02,1.025E-03,9.865E-05,1.129E-05,1.465E-06,2.837E-07,5.323E-08,5.934E-09,0};
-  float bins[nbins+1] = {15,30,50,80,120,170,220,280,370,10000};
+  const int nbins = 8;
+  const int nbinsFine = 120;
+  float bins[nbins+1] = {15,30,50,80,120,170,220,280,10000};
 
-  HiForest *  h = new HiForest("/mnt/hadoop/cms/store/user/dgulhan/pPb/HP04/prod16/Signal_Pythia_pt80/HiForest_v77_v2_merged01/pt80_HP04_hiforest77_hiSignal.root","forest",cPP,1);
+  //pp cross sections for 2.76 tev
+  float crossSection[nbins+1] = {2.034E-01,1.075E-02,1.025E-03,9.865E-05,1.129E-05,1.465E-06,2.837E-07,5.323E-08,0};
+  //pPb cross sections for 5.02 tev
+  if(mode == 1)      
+  {
+    crossSection[0] = 5.335E-01;
+    crossSection[1] = 3.378E-02;
+    crossSection[2] = 3.778E-03;
+    crossSection[3] = 4.412E-04;
+    crossSection[4] = 6.147E-05;
+    crossSection[5] = 1.018E-05;
+    crossSection[6] = 2.477E-06;
+    crossSection[7] = 6.160E-07;
+    crossSection[8] = 0;
+  }
+
+  HiForest * h[nbins];
+
+  for(int file = 0; file < nbins; file++)
+  {
+    if(mode == 0)
+    {
+      h[file] = new HiForest(Form("/mnt/hadoop/cms/store/user/dgulhan/pPb/HP04/prod16/Signal_Pythia_pt%d/HiForest_v77_v2_merged01/pt%d_HP04_hiforest77_hiSignal.root",(int)bins[file],(int)bins[file]),"forest",cPP,1);
+    }
+    else
+    {
+      h[file] = new HiForest(Form("/mnt/hadoop/cms/store/user/dgulhan/pPb/HP04/prod16/Hijing_Pythia_pt%d/HiForest_v77_merged01/pt%d_HP04_prod16_v77_merged_forest_0.root",(int)bins[file],(int)bins[file]),"forest",cPPb,1);
+    }
+    h[file]->LoadNoTrees();
+    h[file]->hasAk3JetTree = true;
+    h[file]->hasSkimTree = true;
+  }
+
+  TH1D * g2tev = new TH1D("g2tev","g2tev",nbinsFine,0,300);
+  TH1D * t2tev = new TH1D("t2tev","t2tev",nbinsFine,0,300);
+  TH1D * pthatDistFine = new TH1D("pthatDistFine","pthatDistFine",nbinsFine,0,300);
+
+  for(int file = 0; file<nbins; file++)
+  {
+    int nEntries = h[file]->GetEntries();
+    for(int i = 1; i<nEntries; i++)
+    {
+      h[file]->GetEntry(i);
+      if(h[file]->ak3PF.pthat > bins[file+1]) continue;
+      pthatDistFine->Fill(h[file]->ak3PF.pthat, crossSection[file]/(float)nEntries);    
+
+      if(h[file]->skim.pcollisionEventSelection ==0 && mode ==0) continue;
+      if(h[file]->skim.pPAcollisionEventSelectionPA ==0 && mode ==1) continue;
+
+      for(int j =0; j < h[file]->ak3PF.nref; j++)
+      {     
+         
+        if(TMath::Abs(h[file]->ak3PF.refparton_flavor[j]) < 901)
+        {
+          t2tev->Fill(h[file]->ak3PF.jtpt[j],crossSection[file]/(float)nEntries);
+          if(h[file]->ak3PF.refparton_flavor[j] == 21) g2tev->Fill(h[file]->ak3PF.jtpt[j],crossSection[file]/(float)nEntries);
+        } 
+      }    
+    }
+  }
+
+  TFile * fout = new TFile("gluonFracs.root","update");
+  TH1D * gFrac2tev;
+  TH1D * gFrac5tev;
+  
+  if (mode == 0)
+  {
+    gFrac2tev = (TH1D*) g2tev->Clone("gFrac2tev");
+    gFrac2tev->Divide(t2tev);
+    gFrac2tev->Write();
+  }
+  else 
+  {
+    gFrac5tev = (TH1D*) g2tev->Clone("gFrac5tev");
+    gFrac5tev->Divide(t2tev);
+    gFrac5tev->Write();
+  }
+  return;
+}
+
+void skim2()
+{
+  TH1D::SetDefaultSumw2();
+  TH2D::SetDefaultSumw2();
+
+  const int nbinsFine = 120;
+
+  HiForest * h = new HiForest("/mnt/hadoop/cms/store/user/velicanu/mergedFF_forests/mc/pp_7TeV/0.root","forest",cPP,1);
   h->LoadNoTrees();
-  h->hasEvtTree = true;
   h->hasAk3JetTree = true;
-  h->hasHltTree = true;
   h->hasSkimTree = true;
 
-  TH1D * g2tev = new TH1D("g2tev","g2tev",100,0,300);
-  TH1D * t2tev = new TH1D("t2tev","t2tev",100,0,300);
-  TH1D * pthatDist = new TH1D("pthatDist","pthatDist",nbins,bins);
-  TH1D * pthatDistFine = new TH1D("pthatDistFine","pthatDistFine",100,0,300);
-  TH1D * weights = new TH1D("weights","weights",nbins,bins);
-
+  TH1D * g7tev = new TH1D("g7tev","g7tev",nbinsFine,0,300);
+  TH1D * t7tev = new TH1D("t7tev","t7tev",nbinsFine,0,300);
 
   int nEntries = h->GetEntries();
-
   for(int i = 1; i<nEntries; i++)
   {
     h->GetEntry(i);
-    if(h->skim.pcollisionEventSelection ==0) continue;
-    pthatDist->Fill(h->ak3PF.pthat);
-  }
-  
-  for(int i = 4; i<nbins; i++)
-  {
-    weights->SetBinContent(i+1,(crossSection[i]-crossSection[i+1])*1000000/((float)pthatDist->GetBinContent(i+1)));
-  }
-  
-  for(int i = 1; i<nEntries; i++)
-  {
-    h->GetEntry(i);
-    
-    if(h->skim.pcollisionEventSelection ==0 ) continue;
-    pthatDistFine->Fill(h->ak3PF.pthat, weights->GetBinContent(weights->FindBin(h->ak3PF.pthat)));    
 
     for(int j =0; j < h->ak3PF.nref; j++)
     {
-      if(h->ak3PF.refparton_flavor[j] > 0)
+      if(TMath::Abs(h->ak3PF.refparton_flavor[j]) < 901)
       {
-        t2tev->Fill(h->ak3PF.refpt[j],weights->GetBinContent(weights->FindBin(h->ak3PF.pthat)));
-        if(h->ak3PF.refparton_flavor[j] == 21) g2tev->Fill(h->ak3PF.refpt[j],weights->GetBinContent(weights->FindBin(h->ak3PF.pthat)));
-      } 
-    }    
+        t7tev->Fill(h->ak3PF.jtpt[j]);
+        if(h->ak3PF.refparton_flavor[j] == 21) g7tev->Fill(h->ak3PF.jtpt[j]);
+      }
+    }
   }
+  
+  TFile * fout = new TFile("gluonFracs.root","update");
+  TH1D *  gFrac7tev = (TH1D*) g7tev->Clone("gFrac7tev");
+  gFrac7tev->Divide(t7tev);
+  gFrac7tev->Write();
 
-  TH1D * gFrac2tev = (TH1D*) g2tev->Clone("gFrac2tev");
-  gFrac2tev->Divide(t2tev);
   return;
 }
