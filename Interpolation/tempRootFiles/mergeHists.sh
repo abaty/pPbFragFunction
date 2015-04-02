16a3dcc690e0095664fc7fd3a4cffa2540601c0a
Austin A Baty <abaty@hidsk0001.cmsaf.mit.edu>
2015-04-02 16:56:37 -0400

    fixed the merging shell script

--- Interpolation/tempRootFiles/mergeHists.sh
+++ Interpolation/tempRootFiles/mergeHists.sh
@@ -9,6 +9,7 @@ hadd $now/Pbp5_UE0_0_15.root spectraPbp5*_0_UE0_0_15.root
 hadd $now/pp7_UE0_0_15.root spectrapp7*_0_UE0_0_15.root
 hadd $now/pp2MC_UE0_0_15.root spectrapp2*_1_UE0_0_15.root
 hadd $now/pPb5MC_UE0_0_15.root spectrapPb5*_1_UE0_0_15.root
+hadd $now/Pbp5MC_UE0_0_15.root spectraPbp5*_1_UE0_0_15.root
 hadd $now/pp7MC_UE0_0_15.root spectrapp7*_1_UE0_0_15.root
 hadd $now/pp5MC_UE0_0_15.root spectrapp5*_1_UE0_0_15.root
 hadd $now/pp2_UE3_0_15.root spectrapp2*_0_UE3_0_15.root
@@ -17,5 +18,6 @@ hadd $now/Pbp5_UE3_0_15.root spectraPbp5*_0_UE3_0_15.root
 hadd $now/pp7_UE3_0_15.root spectrapp7*_0_UE3_0_15.root
 hadd $now/pp2MC_UE3_0_15.root spectrapp2*_1_UE3_0_15.root
 hadd $now/pPb5MC_UE3_0_15.root spectrapPb5*_1_UE3_0_15.root
+hadd $now/Pbp5MC_UE3_0_15.root spectraPbp5*_1_UE3_0_15.root
 hadd $now/pp7MC_UE3_0_15.root spectrapp7*_1_UE3_0_15.root
 hadd $now/pp5MC_UE3_0_15.root spectrapp5*_1_UE3_0_15.root
